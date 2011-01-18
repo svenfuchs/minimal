23b3a605d44144eb15dc731d238e9009ce4c6528
Sven Fuchs <svenfuchs@artweb-design.de>
2011-01-18 12:05:08 +0100

    tighten handler regexp

--- lib/minimal/template/handler.rb
+++ lib/minimal/template/handler.rb
@@ -15,7 +15,7 @@ class Minimal::Template
     protected
 
       def template_class_name_and_format(path)
-        %r(views/(.*?)(\.[^\.]*)?.rb) =~ path
+        %r((?:^|/)views/(.*?)(\.[^\.]*)?.rb) =~ path
         [$1.try(:camelize), ($2 || 'html').gsub('.', '').to_sym]
       end
   end
